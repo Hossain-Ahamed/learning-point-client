2ec2903ff0427f8d34eacb63a3e3791e2039ee70
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-05-01 03:58:18 +0600

    done

--- src/pages/Others/FAQ/FAQ.js
+++ src/pages/Others/FAQ/FAQ.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const FAQ = () => {
-    return (
-        <div>
-            FAQ
-        </div>
-    );
-};
+    function downloadImage(imageUrl) {
+        const link = document.createElement('a');
+        link.href = imageUrl;
+        link.download = 'my-img.jpg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+        return (
+            <div>
+                <img src="https://authentication-project001.web.app/assets/carousel/images/Huge%20supporing%20team.jpeg" alt="" />
+                <button onClick={() => downloadImage('https://authentication-project001.web.app/assets/carousel/images/Huge%20supporing%20team.jpeg')}>download </button>
+            </div>
+        );
+    };
 
-export default FAQ;
+    export default FAQ;
